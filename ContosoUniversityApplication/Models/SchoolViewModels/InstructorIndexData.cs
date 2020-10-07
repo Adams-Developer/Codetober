e4731af168fefa8b29d9d7f397a8af4039a2acea
Tiffany Adams <tadams03@mail.roosevelt.edu>
2020-10-07 16:07:16 -0500

    Add Instructors page

---
+++ ContosoUniversityApplication/Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+﻿using ContosoUniversityApplication.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityApplication.Models.SchoolViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
