4fd88b4b866795af8f240c2379615e72c19411f6
Tiffany Adams <tadams03@mail.roosevelt.edu>
2020-10-15 21:20:02 -0500

    Add controller/UI

---
+++ ChronicKidneyDiseasePredictionApplication/Controllers/KidneyDiseasePredictionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChronicKidneyDiseasePredictionApplicationML.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ChronicKidneyDiseasePredictionApplication.Controllers
+{
+    public class KidneyDiseasePredictionController : Controller
+    {
+        private readonly ILogger<KidneyDiseasePredictionController> _logger;
+
+        public KidneyDiseasePredictionController(ILogger<KidneyDiseasePredictionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ModelInput input)
+        {
+            ModelOutput prediction = ConsumeModel.Predict(input);
+            ViewBag.Result = prediction;
+
+            return View();
+        }
+    }
+}
--- ChronicKidneyDiseasePredictionApplication/Startup.cs
+++ ChronicKidneyDiseasePredictionApplication/Startup.cs
@@ -50,7 +50,7 @@ namespace ChronicKidneyDiseasePredictionApplication
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=KidneyDiseasePrediction}/{action=Index}/{id?}");
             });
         }
     }
